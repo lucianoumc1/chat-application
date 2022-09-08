e966a2ae3c6bf17e0f1f01b240d028795d668e8c
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-09-08 02:41:36 -0300

    [FIX] auth services

--- src/services/authWithEmailAndPassword.js
+++ src/services/authWithEmailAndPassword.js
@@ -2,13 +2,31 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { Auth } from "../contexts/FirebaseContext/FirebaseApp";
+import { query, doc, setDoc } from "firebase/firestore";
+import { Auth, db } from "../contexts/FirebaseContext/FirebaseApp";
 
 export default function authWithEmailAndPassword() {
+  const saveUser = (user) => {
+    const userId = user.email.split(/@/)[0];
+
+    const querySaveChat = query(doc(db, "users", user.uid));
+    const docData = {
+      id: user.uid,
+      user_name: user.name,
+      user_id: userId,
+      avatar: user.avatar,
+    };
+    setDoc(querySaveChat, docData)
+      .then((res) => console.log(res))
+      .catch((e) => console.error(e.message));
+  };
+
   const createUser = (data) => {
     createUserWithEmailAndPassword(Auth, data.email, data.password)
       .then((userCredential) => {
         const { user } = userCredential;
+        const newData = { ...data, uid: user.uid };
+        saveUser(newData);
         return user;
       })
       .catch((error) => {
--- src/services/authWithPopup.js
+++ src/services/authWithPopup.js
@@ -1,8 +1,24 @@
 import { signInWithPopup, signOut } from "firebase/auth";
-import { Auth } from "../contexts/FirebaseContext/FirebaseApp";
+import { query, doc, setDoc } from "firebase/firestore";
+import { Auth, db } from "../contexts/FirebaseContext/FirebaseApp";
 
 export default function authWithPopup() {
-  const logIn = (provider) => signInWithPopup(Auth, provider);
+  const saveUser = (user) => {
+    const querySaveChat = query(doc(db, "users", user.uid));
+    const docData = {
+      id: user.uid,
+      user_name: user.displayName,
+      user_id: user.email.replace("@gmail.com", ""),
+      avatar: user.reloadUserInfo.photoUrl,
+    };
+    setDoc(querySaveChat, docData).catch((e) => console.error(e.message));
+  };
+
+  const logIn = (provider) =>
+    signInWithPopup(Auth, provider).then((result) => {
+      const { user } = result;
+      saveUser(user);
+    });
 
   const logOut = () => signOut(Auth);
 
