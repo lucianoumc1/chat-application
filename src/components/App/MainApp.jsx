e4d11bc877de58dd24da11c0e725914a0788a649
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-09-09 10:24:18 -0300

    [ADD] react router

--- src/components/App/MainApp.jsx
+++ src/components/App/MainApp.jsx
@@ -2,14 +2,15 @@ import { useContext } from "react";
 import { FirebaseContext } from "../../contexts/FirebaseContext";
 import { ChatsList } from "../ChatsList";
 import { ChatRoom } from "../ChatRoom";
-import Login from "../views/Login";
+// import Login from "../views/Login";
+import Register from "../views/Register";
 
 export function MainApp() {
   const { account } = useContext(FirebaseContext);
 
   return (
     <main className="main-app">
-      {(!account && <Login />) || (
+      {(!account && <Register />) || (
         <>
           <ChatsList />
           <ChatRoom />
