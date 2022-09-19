3de12b69383a7ef78f2ec45337382faa32c6c05e
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-09-19 15:31:08 -0300

    [FIX] chatRoom styles

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -1,4 +1,4 @@
-import "./Chat.css";
+import "./ChatRoom.css";
 import { useContext } from "react";
 import { MessageSendingForm } from "../MessageSendingForm";
 import { MessageReceived } from "../MessageReceived";
@@ -10,21 +10,31 @@ export function ChatRoom() {
   const { chatMessages, account, chatId } = useContext(FirebaseContext);
 
   return (
-    <div className="chat__container">
-      <HeaderChatRoom uImage={chatId.uImage} userName={chatId.name} />
-      <div className="message-room__message-container">
-        {chatId && (
-          <>
+    <div className="chat-room__container">
+      {!chatId && (
+        <div className="chat-room__cover-default">
+          <h2 className="chat-room__title">Welcome!</h2>
+          <div className="chat-room__description">
+            Send messages to your friends easily. <br /> If you do not have
+            contacts try creating a chat with <strong>lucianoumc1</strong>
+          </div>
+        </div>
+      )}
+
+      {chatId && (
+        <>
+          <HeaderChatRoom uImage={chatId.uImage} userName={chatId.name} />
+          <div className="chat-room__messages-container">
             {chatMessages.map(
               (el) =>
                 (el.sender_id === account.id && (
                   <MessageSent message={el.text} key={el.id} />
                 )) || <MessageReceived message={el.text} key={el.id} />
             )}
-          </>
-        )}
-      </div>
-      <MessageSendingForm />
+          </div>
+          <MessageSendingForm />
+        </>
+      )}
     </div>
   );
 }
